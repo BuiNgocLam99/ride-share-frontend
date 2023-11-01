a1338bfc4c38a4a212f100099d01cdff471e47cd
lambn <lambn957@gmai.com>
2023-11-01 16:47:42 +0700

    make login and check token user

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="min-screen bg-gray-100 text-center antialiased">
+    <RouterView />
+  </div>
+</template>
